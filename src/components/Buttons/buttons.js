6a03dfc6c3ee73bdd74a2aeaf97847ea8b03e6d0
Andrew Schubert <aschubert@qca-sdc.com>
2018-07-24 10:20:10 -0500

    add onClick handler to buttons

--- src/components/Buttons/buttons.js
+++ src/components/Buttons/buttons.js
@@ -2,13 +2,32 @@ import React from 'react';
 
 const Buttons = (props) => {
 
+  const handleClick = (e) => {
+    const buttonType = e.target.name;
+
+    switch(buttonType) {
+      case 'next': 
+        alert(`dispatch action to update ${buttonType}!`)
+      case 'previous': 
+        alert(`dispatch action to update ${buttonType}!`)
+      case 'like': 
+        alert(`dispatch action to update ${buttonType}!`)
+      case 'NSFW': 
+        alert(`dispatch action to update ${buttonType}!`)
+      case 'slack': 
+        alert(`dispatch action to update ${buttonType}!`)
+      default:
+        return
+    }
+  }
+  
   return (
     <section className="user-buttons">
-      <button className="like-button" name='like'>Like</button>
-      <button className="NSFW-button" name='NSFW'>NSFW</button>
-      <button className="previous-button" name='previous'>Previous</button>
-      <button className="next-button" name='next'>Next</button>
-      <button className="slack-button" name='slack'>Share To Slack</button>
+      <button className="like-button" name='like' onClick={handleClick}>Like</button>
+      <button className="NSFW-button" name='NSFW' onClick={handleClick}>NSFW</button>
+      <button className="previous-button" name='previous' onClick={handleClick}>Previous</button>
+      <button className="next-button" name='next' onClick={handleClick}>Next</button>
+      <button className="slack-button" name='slack' onClick={handleClick}>Share To Slack</button>
     </section>
   )
 }
