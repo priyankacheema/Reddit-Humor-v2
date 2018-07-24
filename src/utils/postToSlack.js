17233f7a56545bfd8351159932a455cce6a5d1f8
Forrest Carlton <fcarlton@qca-sdc.com>
2018-07-24 10:03:21 -0500

    feat: add postToSlack util function

---
+++ src/utils/postToSlack.js
@@ -0,0 +1,20 @@
+var urlWebHook = "https://hooks.slack.com/services/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";
+
+export default function sendToSlack (message = '', imageUrl = undefined ) {
+	let payload = {
+        text: message,
+        attachments: [
+            {
+            "fallback": "this image failed to post",
+            "text": `${message}`,
+            "image_url": `${imageUrl}`,
+            "thumb_url": `${imageUrl}`
+            }
+        ]
+		}
+
+	fetch(urlWebHook, {
+        method: 'post',
+        body: JSON.stringify(payload)
+    })
+	}
