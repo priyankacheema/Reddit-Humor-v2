e12793430a5b6be81ce534e9f6265d1ae20215a3
swatkins <swatkins@aquent.com>
2018-07-25 10:00:02 -0500

    feat: Add interval to fetch images from Reddit

--- src/components/Display/display.js
+++ src/components/Display/display.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Buttons from '../Buttons/connectedButtons';
 import DisplayImage from '../DisplayImage/connectedDisplayImage';
 
 class Display extends Component {
 
-  componentDidMount() {
-    this.props.getHumor()
-  }
 
-  render() {
-    return (
-      <section className="display">
-        <Buttons />
-        <DisplayImage />
-      </section>
-    )
-  }
+    componentDidMount() {
+        this.getHumorEvery(1000)
+    }
+
+    render() {
+        return (
+            <section className="display">
+                <Buttons/>
+                <DisplayImage/>
+            </section>
+        )
+    }
+
+    getHumorEvery = (ms) => {
+
+        this.props.getHumor()
+        setInterval(() => {
+
+            this.props.getHumor()
+
+        }, ms)
+
+    }
+
 }
 
 Display.propTypes = {
-  getHumor: PropTypes.func.isRequired
+    getHumor: PropTypes.func.isRequired
 }
 
 export default Display;
