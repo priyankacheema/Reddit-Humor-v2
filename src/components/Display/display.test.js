ec7a609cf3d4b823e608aba9b3c6681fcb3e9077
Andrew Schubert <aschubert@qca-sdc.com>
2018-07-30 12:45:14 -0500

    test: add tests for setTimeout and setInterval items of Display

--- src/components/Display/display.test.js
+++ src/components/Display/display.test.js
@@ -4,6 +4,8 @@ import {expect} from 'code';
 import {shallow} from 'enzyme';
 import sinon from 'sinon'
 
+import {getNextIndex} from '../../utils/getNextIndex'
+
 describe('Given Display', () => {
 
     function requiredProps(overRides = {}) {
@@ -16,7 +18,7 @@ describe('Given Display', () => {
             likes: {1: 0, 2: 0, 3: 0},
             getHumor: sinon.spy(),
             setCurrentIndex: sinon.spy(),
-
+            getNextIndex
         };
 
         return {...props, ...overRides}
@@ -40,5 +42,92 @@ describe('Given Display', () => {
         const component = renderComponent();
         expect(component.find('Connect(DisplayImage)').exists()).to.be.true();
     })
+
+    describe('Fetching Images', () => {
+        let clock, getHumorSpy;
+
+        beforeEach(() => {
+            clock = sinon.useFakeTimers();
+            getHumorSpy = sinon.spy();
+        })
+
+        afterEach(() => {
+            sinon.restore()
+        })
+
+        it('should fetch images when the component first mounts', () => {
+            renderComponent(requiredProps({ getHumor: getHumorSpy }));
+            sinon.assert.calledOnce(getHumorSpy)
+        })
+
+        it('should refetch images from Reddit every 3600000 ms', async () => {
+            renderComponent(requiredProps({ getHumor: getHumorSpy }));
+            await Promise.resolve();
+    
+            clock.tick(3600000)
+    
+            sinon.assert.calledTwice(getHumorSpy)
+        })
+    
+    })
+
+    describe('Displaying next Image', () => {
+        let clock, setCurrentIndexSpy, images;
+
+        beforeEach(() => {
+            clock = sinon.useFakeTimers(); 
+            setCurrentIndexSpy = sinon.spy();
+
+            images = [
+                { id: '1', url: 'http://www.fake.com', title: 'pic1', gif: false },
+                { id: '2', url: 'http://www.fake2.com', title: 'pic2', gif: false },
+                { id: '3', url: 'http://www.fake3.com', title: 'pic3', gif: false }
+            ]
+        })
+
+        afterEach(() => {
+            sinon.restore();
+        })
+
+        it('should render a new image every 20000 ms', async () => { 
+            const component = renderComponent(requiredProps({ 
+                images, 
+                setCurrentIndex: setCurrentIndexSpy
+            }));
+            
+            component.setState()
+            await Promise.resolve()
+    
+            clock.tick(20000);
+
+            sinon.assert.calledOnce(setCurrentIndexSpy)
+            sinon.assert.calledWithExactly(setCurrentIndexSpy, 1)
+        })
+    
+        it('if gif, should render a new image after displaying the gif for three times', async () => {
+            const gifDuration = 10000;
+            images = [
+                { id: '1', url: 'http://www.fake.com', title: 'pic1', gif: true },
+                { id: '2', url: 'http://www.fake2.com', title: 'pic2', gif: true },
+                { id: '3', url: 'http://www.fake3.com', title: 'pic3', gif: true }
+            ]
+
+            const component = renderComponent(requiredProps({ 
+                images, 
+                setCurrentIndex: setCurrentIndexSpy, 
+                getGifDuration: sinon.stub().returns(gifDuration)
+            }));
+            
+            component.setState()
+            await Promise.resolve()
+
+            clock.tick(gifDuration * 3);
+    
+            sinon.assert.calledOnce(setCurrentIndexSpy)
+            sinon.assert.calledWithExactly(setCurrentIndexSpy, 1)
+        })
+    })
+
+    
 
 })
--- src/utils/gifDuration.js
+++ src/utils/gifDuration.js
@@ -1,6 +1,6 @@
 import gifyParse from 'gify-parse'; 
 
-export default async function getGifDuration(url) {
+export async function getGifDuration(url) {
     const data = await fetch(url)
     const buffer = await data.arrayBuffer()
     const pictureDataInBinary = Buffer.from(buffer, 'base64')
